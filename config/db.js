1af1d33bcac93bddbcddf5eb9465a07d4f088e7c
Anna <tech.akarfourwheel@gmail.com>
2025-09-23 15:44:33 +0530

    production ready

--- config/db.js
+++ config/db.js
@@ -10,11 +10,11 @@ const caCert = await fs.readFile(path.join(__dirname, '../certs/ca.pem'));
 
 
 const pool = mysql.createPool({
-        host: process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || 'root',
-        database: process.env.DB_NAME || 'akar_db',
-        port: process.env.DB_PORT || 3306,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0,
